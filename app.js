f0f8db5de6fada6f4ecb3192ee4d3b39599389ed
narendra67 <mvnarendrareddy@gmail.com>
2017-10-26 16:49:45 +0530

    models folder with respective models created

--- app.js
+++ app.js
@@ -2,15 +2,11 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
+Campground = require("./models/campground");
+
 mongoose.connect('mongodb://localhost/yelp_camp');
 
-var campgroundSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    description: String
-});
 
-var Campground = mongoose.model("Campground", campgroundSchema);
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
@@ -18,22 +14,6 @@ app.set('view engine', 'ejs');
 app.get('/', function(req, res){
     res.render("landing");
 });
-
-// Campground.create({
-//     name:'Salmon Creek',
-//     image:"https://upload.wikimedia.org/wikipedia/commons/0/05/Biskeri-_Camping_I_IMG_7238.jpg"
-// }, function(err, camp){
-//     if(err){
-//         console.log(err);
-//     }else{
-//         console.log(camp);
-//     }
-// });
-// var campgrounds = [
-//     {name:'Salmon Creek', image:"https://upload.wikimedia.org/wikipedia/commons/0/05/Biskeri-_Camping_I_IMG_7238.jpg"},
-//     {name:'Granite Hill', image:"http://www.blog.weekendthrill.com/wp-content/uploads/2016/07/071416_1116_25AwesomeCa21.png"},
-//     {name:'Mountain Rest', image:"https://www.caravancampingsites.co.uk/broomfield.jpg"}
-// ];
 
 app.get("/campgrounds", function(req, res){
     Campground.find({}, function(err, camp1){
